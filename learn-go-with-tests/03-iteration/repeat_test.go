15ec47e4601d128313f3d177edf72b1c6649bdae
Nicolás Sañudo <nicolas.sanudo2@gmail.com>
2023-08-16 09:49:15 -0300

    ✨ FEAT(go fundamentals): enhance Repeat() with user-defined repetition
    - Updated associated test to reflect changes
    - Added ExampleRepeat() function for usage demonstration

--- learn-go-with-tests/03-iteration/repeat_test.go
+++ learn-go-with-tests/03-iteration/repeat_test.go
@@ -1,18 +1,38 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat 5 times a character", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("user specify how many times the character is repeated", func(t *testing.T) {
+		repeated := Repeat("a", 7)
+		expected := "aaaaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 10)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("n", 3)
+	fmt.Println(repeated)
+	// Output: nnn
 }
